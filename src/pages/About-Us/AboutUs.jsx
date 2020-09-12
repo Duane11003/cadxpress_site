474fe6ff002ce2c850ce6a9df05adad871c5d77f
Duane McFarlane <duane.mcfar@gmail.com>
2020-09-12 17:53:54 -0400

    added responsive css

--- src/pages/About-Us/AboutUs.jsx
+++ src/pages/About-Us/AboutUs.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import '../About-Us/About-Us.scss'
 
 const AboutUs = () => {
     return (
-        <div className='outer-container'>
-            <div className='inner-container'>
-              <h1>Operating Principles</h1>
-              <h2>Reliability</h2>
+        <div className='about-us-outer-container'>
+            <div className='about-us-inner-container'>
+              <h1 className='about-us-header-1'>Operating Principles</h1>
+              <h2 className='about-us-header-2'>Reliability</h2>
               <p>We're road warriors and even pickup and drop off on holidays. We've found that the best way to do business is to pretend the shipment we've picked up needs to be dropped off yesterday. Putting your needs first is the bridge to building lasting relationships.</p>
             </div>
-            <div className='inner-container'>
-              <h2>Communication/Integrity</h2>
+            <div className='about-us-inner-container'>
+              <h2 className='about-us-header-2'>Communication/Integrity</h2>
               <p>The essense of business is beingn able to communicate with your clients at the moment something doesn't go according to plan and not waiting until the last moment. Trust is key and our goal is to establish trust not just with our words, but with our actions.</p>
             </div>
-            <div className='inner-container'>
-              <h2>True Partnership</h2>
+            <div className='about-us-inner-container'>
+              <h2 className='about-us-header-2'>True Partnership</h2>
               <p>We're in this together and the understanding that if your load doesn't get delivered on time affects us just as much as it affects you, so if you need a freight hauled with no stress? Say less and choose Cadxpress!</p>
             </div>
         </div>
--- src/pages/Landing-Page/Landing-Page.jsx
+++ src/pages/Landing-Page/Landing-Page.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import './Landing-Page.scss'
+import AboutUs from '../About-Us/AboutUs.jsx'
+import Services from '../Services/Services.jsx'
+import Contact from '../Contact/Contact.jsx'
+
+
 
 const Landing = () => {
     return (
-        <div className='container'>
-            <div className='inner-container'>
-                <h1 className='header-1'>Got freight? We haul!</h1>
-                <p className='body-text'>No matter how close or far, we'll get the job done. Licensed to pick up and drop off shipments <span className='bold'>nationwide</span>. Our transportation services include dry goods, refrigerated loads, half-loads and full-loads. If you need freight hauled with no stress? Say less and choose Cadxpress!</p>
+        <Fragment>
+            <div className='container'>
+                <div className='inner-container'>
+                    <h1 className='header-1'>Got freight? We haul!</h1>
+                    <p className='body-text'>No matter how close or far, we'll get the job done. Licensed to pick up and drop off shipments <span className='bold'>nationwide</span>. Our transportation services include dry goods, refrigerated loads, half-loads and full-loads. If you need freight hauled with no stress? Say less and choose Cadxpress!</p>
+                </div>
             </div>
-        </div>
+            <AboutUs />
+            <Services />
+            <Contact />
+        </Fragment>
     )
 }
 
